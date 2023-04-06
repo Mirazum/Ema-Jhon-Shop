7c80dc80ef4bed32238913f79e27f2cd233ba2d9
Mirazum Parvin <mira.sust@gmail.com>
2023-04-06 11:53:01 +0300

    more implimentation added

--- src/Loaders/cartProductsLoader.js
+++ src/Loaders/cartProductsLoader.js
@@ -1,8 +1,25 @@
+import { getShoppingCart } from "../../utilities/fakedb";
+
 const cartProductLoader = async()=>{
     const loaderProduct =await fetch('products.json');
     const products= await loaderProduct.json();
+    const storedCart = getShoppingCart();
+    const savedCart =[];
+    for(const id in storedCart){
+        const addedProduct = products.find(pd=>pd.id===id);
+        const quantity = storedCart[id];
+        addedProduct.quantity= quantity
+        savedCart.push(addedProduct)
+    }
+
+
+
+
+
+
+
     console.log(products)
-    return products
+    return savedCart
 
 }
 
